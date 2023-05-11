7de7c9fd5c6a69f9d8ad5804844a383b98faed33
mariammaher550 <mariammaher550@gmail.com>
2023-05-11 21:23:18 +0300

    updated streamlit, stage 1 and stage 2

--- UPLOAD.sh
+++ UPLOAD.sh
@@ -1,3 +1,3 @@
 dos2unix scripts/*
 # scp -P 2222 main.sh root@localhost:/root/big-data-project
-scp -P 2222 scripts/* root@localhost:/root/big-data-project/scripts
+scp -P 2222 scripts/* root@localhost:/root/bigdata-final-project-iu-2023/scripts
