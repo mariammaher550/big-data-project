d41c5cfd23d55e3484307af8a4250baf911c3c02
MohamedHamdy28 <71794972+MohamedHamdy28@users.noreply.github.com>
2023-05-11 22:01:16 +0300

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# echo "installing requirements in hdfs"
-# pip install -r requirements.txt --ignore-installed
+echo "installing requirements in hdfs"
+pip install -r requirements.txt --ignore-installed
 python scripts/preprocessing.py
