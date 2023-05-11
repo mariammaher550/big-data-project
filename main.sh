0fc18f4c04a0a98b5b80a06739a8d1ec50867751
MohamedHamdy28 <71794972+MohamedHamdy28@users.noreply.github.com>
2023-05-11 21:59:29 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Pre-processing
-#echo "Running pre-processing"
-#bash scripts/preprocess.sh
+echo "Running pre-processing"
+bash scripts/preprocess.sh
 
 # Run the big data pipeline
 echo "Running Stage 1 of the pipeline - PostgreSQL + Sqoop"
@@ -11,15 +11,15 @@ bash scripts/stage1.sh
 echo "Running Stage 2 of the pipeline - Hive + Spark SQL"
 bash scripts/stage2.sh
 
-# echo "Running Stage 3 of the pipeline - Spark ML"
-# bash scripts/stage3.sh
+echo "Running Stage 3 of the pipeline - Spark ML"
+bash scripts/stage3.sh
 
 echo "Running Stage 4 of the pipeline - Streamlit"
 bash scripts/stage4.sh
-#
-# Post-processing
-# echo "Running post-processing!"
-# bash scripts/postprocess.sh
+
+# Post-processing 
+echo "Running post-processing!"
+bash scripts/postprocess.sh
 
 
 # Check the quality of the codes
